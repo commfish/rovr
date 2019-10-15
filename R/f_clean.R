4ea5390390b29e9436228a8e3725175f1a000e7e
Ben <ben.williams@alaska.gov>
2019-10-15 08:54:36 -0800

    init commit

---
+++ R/f_clean.R
@@ -0,0 +1,17 @@
+#' f_clean
+#'
+#' @param data
+#'
+#' @return
+#' @export f_clean
+#'
+#' @examples
+f_clean <- function(data){
+  data %>%
+    mutate(Dive = as.numeric(gsub(".*_", "", DIVE_NO))) %>%
+    full_join(qc_sseo) %>%
+    fill(Family) %>%
+    filter(!is.na(Family))%>%
+    dplyr::select(Seconds = SECONDS, Dive, ROV_X, ROV_Y, Family) %>%
+    mutate(Dive = factor(Dive))
+}
